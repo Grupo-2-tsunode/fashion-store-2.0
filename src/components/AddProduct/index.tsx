a86f3d5bd59dab6eee53650c271cc43dffb4a652
lauremiro <lauremiromelo@gmail.com>
2023-07-06 10:24:26 -0300

    atualização modal

--- src/components/AddProduct/index.tsx
+++ src/components/AddProduct/index.tsx
@@ -4,6 +4,7 @@ import { AddProductStyled } from './styled'
 import { ButtonStyled } from '../../styles/ButtonStyled'
 import { useContext } from 'react'
 import { AdmContext } from '../../providers/AdmContext/AdmContext'
+import { InputComponent } from '../Input'
 
 export const AddProduct = () => {
 
@@ -22,9 +23,10 @@ export const AddProduct = () => {
                         <AiOutlineClose />
                     </span>
                 </div>
-                <input type="text" name="" id="" />
-                <input type="number" name="" id="" />
-                <input type="text" name="" id="" />
+                <InputComponent placeholder='NOME' type='text'/>
+                <InputComponent placeholder='PREÇO (R$)' type='number'/>
+                <InputComponent placeholder='IMAGEM(URL)' type='text'/>
+                <InputComponent placeholder='DESCRIÇÃO RESUMIDA' type='text'/>
                 <textarea name="" id=""></textarea>
                 <div className='buttonAdd'>
                     <ButtonStyled buttonStyle='black'>
